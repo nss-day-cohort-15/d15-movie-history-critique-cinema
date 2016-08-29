110c7b3ec93d7cf2c302ac4e7726023725d338bd
Thomas J. Shavor <thomasshavor@gmail.com>
2016-08-29 13:40:17 -0500

    dom builder and handlebars changes

--- js/dom-builder.js
+++ js/dom-builder.js
@@ -1,20 +1,20 @@
 "use strict";
 
-function populateUserMovies(fbmovieData){
-  console.log("I'll populate the user's movies from FB");
+function populateUserMovies(fbmovieData) {
+    console.log("I'll populate the user's movies from FB");
 
 
 }
 
-function populateNewMovie(newMovieObj){
-  console.log("I'll put this into the DOM ", newMovieObj);
-  $(".movieOutput").html(`<h2>${newMovieObj.movieTitle}</h2><h4>${newMovieObj.movieYear}</h4><h4>${newMovieObj.movieActors}</h4><button class="addToUnwatched">Add this to your unwatched list</button><button class="addToWatched">add this to your already watched list</button>`);
+function populateNewMovie(newMovieObj) {
+    console.log("I'll put this into the DOM ", newMovieObj);
+    $(".movieOutput").html(`<h2>${newMovieObj.movieTitle}</h2><h4>${newMovieObj.movieYear}</h4><h4>${newMovieObj.movieActors}</h4><button class="addToUnwatched">Add this to your unwatched list</button><button class="addToWatched">add this to your already watched list</button>`);
 }
 
 
 
 module.exports = {
-  populateUserMovies,
-  populateNewMovie
+    populateUserMovies,
+    populateNewMovie
 
 };
